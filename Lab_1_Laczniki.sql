450e019ab8000af3a5ee581fb9640d2d5a089be2
dominik-maj9 <stillglossa@gmail.com>
2026-09-24 10:35:15 +0200

    Update Lab_1_Laczniki.sql

--- Lab_1_Laczniki.sql
+++ Lab_1_Laczniki.sql
@@ -1,21 +1,15 @@
-
--- Zadanie 3: stworzenie link do bazalib
-
+ -- Zadanie 3 tworzynie link do baza11b
 CREATE DATABASE LINK dblinkFilia
   CONNECT TO RBD1_ST20
   IDENTIFIED BY start123
   USING 'baza11b';
  
  
-
 -- Zadanie 4
-
 SELECT * FROM kursanci@dblinkFilia;
  
  
-
 -- Zadanie 5
-
 CREATE OR REPLACE SYNONYM kursanciSiedziba   FOR kursanci;
 CREATE OR REPLACE SYNONYM kursySiedziba      FOR kursy;
 CREATE OR REPLACE SYNONYM rodzajeSiedziba    FOR rodzaje;
@@ -28,7 +22,6 @@ CREATE OR REPLACE SYNONYM wykladowcyFilia    FOR wykladowcy@dblinkFilia;
  
  
 -- Zadanie 6
-
 CREATE OR REPLACE VIEW kursanciAll AS
 SELECT imie, nazwisko FROM kursanciSiedziba
 UNION
@@ -39,131 +32,51 @@ SELECT imie, nazwisko FROM wykladowcySiedziba
 UNION
 SELECT imie, nazwisko FROM wykladowcyFilia;
  
-
+ 
 -- Zadanie 7
 
 CREATE OR REPLACE VIEW kursyAll AS
-SELECT k.kurs_id,
-       r.nazwa               AS nazwa_kursu,
-       w.imie,
-       w.nazwisko,
-       COUNT(u.kursant_id)   AS ilosc_uczestnikow
+SELECT k.kurs_id, r.nazwa AS nazwa_kursu, w.imie, w.nazwisko,
+       COUNT(u.kursant_id) AS ilosc_uczestnikow,
+       r.cena, r.godz, w.stawka
 FROM kursySiedziba k
-JOIN rodzajeSiedziba    r ON r.rodzaj_id     = k.rodzaj_id
+JOIN rodzajeSiedziba r    ON r.rodzaj_id = k.rodzaj_id
 JOIN wykladowcySiedziba w ON w.wykladowca_id = k.wykladowca_id
-LEFT JOIN umowy         u ON u.kurs_id       = k.kurs_id
-GROUP BY k.kurs_id, r.nazwa, w.imie, w.nazwisko
+LEFT JOIN umowy u         ON u.kurs_id = k.kurs_id
+GROUP BY k.kurs_id, r.nazwa, w.imie, w.nazwisko, r.cena, r.godz, w.stawka
 UNION ALL
-SELECT k.kurs_id,
-       r.nazwa,
-       w.imie,
-       w.nazwisko,
-       COUNT(u.kursant_id)
+SELECT k.kurs_id, r.nazwa, w.imie, w.nazwisko,
+       COUNT(u.kursant_id),
+       r.cena, r.godz, w.stawka
 FROM kursyFilia k
-JOIN rodzajeFilia    r ON r.rodzaj_id     = k.rodzaj_id
+JOIN rodzajeFilia r    ON r.rodzaj_id = k.rodzaj_id
 JOIN wykladowcyFilia w ON w.wykladowca_id = k.wykladowca_id
-LEFT JOIN umowy      u ON u.kurs_id       = k.kurs_id
-GROUP BY k.kurs_id, r.nazwa, w.imie, w.nazwisko;
+LEFT JOIN umowy u      ON u.kurs_id = k.kurs_id
+GROUP BY k.kurs_id, r.nazwa, w.imie, w.nazwisko, r.cena, r.godz, w.stawka;
  
 SELECT * FROM kursyAll;
  
  
+-- Zadanie 8
 
-
--- Zadanie 8: przychód ze wszystkich aktualnie prowadzonych kursów
-
-WITH kursy_all AS (
-    SELECT k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka,
-           COUNT(u.kursant_id) AS ilosc
-    FROM kursySiedziba k
-    JOIN rodzajeSiedziba    r ON r.rodzaj_id     = k.rodzaj_id
-    JOIN wykladowcySiedziba w ON w.wykladowca_id = k.wykladowca_id
-    LEFT JOIN umowy         u ON u.kurs_id       = k.kurs_id
-    GROUP BY k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka
-    UNION ALL
-    SELECT k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka,
-           COUNT(u.kursant_id)
-    FROM kursyFilia k
-    JOIN rodzajeFilia    r ON r.rodzaj_id     = k.rodzaj_id
-    JOIN wykladowcyFilia w ON w.wykladowca_id = k.wykladowca_id
-    LEFT JOIN umowy      u ON u.kurs_id       = k.kurs_id
-    GROUP BY k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka
-)
-SELECT SUM(ilosc * cena) AS przychod_razem
-FROM kursy_all;
+SELECT SUM(ilosc_uczestnikow * cena) AS przychod
+FROM kursyAll;
  
  
-
 -- Zadanie 9
 
-WITH kursy_all AS (
-    SELECT k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka,
-           COUNT(u.kursant_id) AS ilosc
-    FROM kursySiedziba k
-    JOIN rodzajeSiedziba    r ON r.rodzaj_id     = k.rodzaj_id
-    JOIN wykladowcySiedziba w ON w.wykladowca_id = k.wykladowca_id
-    LEFT JOIN umowy         u ON u.kurs_id       = k.kurs_id
-    GROUP BY k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka
-    UNION ALL
-    SELECT k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka,
-           COUNT(u.kursant_id)
-    FROM kursyFilia k
-    JOIN rodzajeFilia    r ON r.rodzaj_id     = k.rodzaj_id
-    JOIN wykladowcyFilia w ON w.wykladowca_id = k.wykladowca_id
-    LEFT JOIN umowy      u ON u.kurs_id       = k.kurs_id
-    GROUP BY k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka
-)
-SELECT SUM(godz * stawka) AS koszty_razem
-FROM kursy_all;
+SELECT SUM(godz * stawka) AS koszty
+FROM kursyAll;
  
  
-
 -- Zadanie 10
-
-WITH kursy_all AS (
-    SELECT k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka,
-           COUNT(u.kursant_id) AS ilosc
-    FROM kursySiedziba k
-    JOIN rodzajeSiedziba    r ON r.rodzaj_id     = k.rodzaj_id
-    JOIN wykladowcySiedziba w ON w.wykladowca_id = k.wykladowca_id
-    LEFT JOIN umowy         u ON u.kurs_id       = k.kurs_id
-    GROUP BY k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka
-    UNION ALL
-    SELECT k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka,
-           COUNT(u.kursant_id)
-    FROM kursyFilia k
-    JOIN rodzajeFilia    r ON r.rodzaj_id     = k.rodzaj_id
-    JOIN wykladowcyFilia w ON w.wykladowca_id = k.wykladowca_id
-    LEFT JOIN umowy      u ON u.kurs_id       = k.kurs_id
-    GROUP BY k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka
-)
-SELECT kurs_id,
-       nazwa                          AS nazwa_kursu,
-       ilosc * cena                   AS przychod,
-       godz * stawka                  AS koszt,
-       ilosc * cena - godz * stawka   AS zysk
-FROM kursy_all
+SELECT kurs_id, nazwa_kursu,
+       ilosc_uczestnikow * cena - godz * stawka AS zysk
+FROM kursyAll
 ORDER BY kurs_id;
  
-
--- Zadanie 11 
-
-WITH kursy_all AS (
-    SELECT k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka,
-           COUNT(u.kursant_id) AS ilosc
-    FROM kursySiedziba k
-    JOIN rodzajeSiedziba    r ON r.rodzaj_id     = k.rodzaj_id
-    JOIN wykladowcySiedziba w ON w.wykladowca_id = k.wykladowca_id
-    LEFT JOIN umowy         u ON u.kurs_id       = k.kurs_id
-    GROUP BY k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka
-    UNION ALL
-    SELECT k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka,
-           COUNT(u.kursant_id)
-    FROM kursyFilia k
-    JOIN rodzajeFilia    r ON r.rodzaj_id     = k.rodzaj_id
-    JOIN wykladowcyFilia w ON w.wykladowca_id = k.wykladowca_id
-    LEFT JOIN umowy      u ON u.kurs_id       = k.kurs_id
-    GROUP BY k.kurs_id, r.nazwa, r.cena, r.godz, w.stawka
-)
-SELECT SUM(ilosc * cena - godz * stawka) AS laczny_zysk
-FROM kursy_all;
+ 
+-- Zadanie 11
+SELECT SUM(ilosc_uczestnikow * cena - godz * stawka) AS laczny_zysk
+FROM kursyAll;
+ 
